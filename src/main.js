85f17d64c5d224f1335271e9ff470bcf6486c3c0
Maor Shushan <mshushan45@gmail.com>
2022-05-24 09:56:36 +0300

    Added Router

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import './plugins/element.js'
 
 import { firestorePlugin } from 'vuefire'
 import vuetify from './plugins/vuetify'
+import router from './router'
 Vue.use(firestorePlugin);
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
