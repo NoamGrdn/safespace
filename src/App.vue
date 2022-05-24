ba5fe3907d006f6e4ddd3df93572c5eb0c9d3982
Maor Shushan <mshushan45@gmail.com>
2022-05-24 09:55:45 +0300

    Vuetify Added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,60 @@
 <template>
-<!-- eslint-disable -->
-    <tbody>
-      <tr v-for="user in usersData">
-        <td>{{user.name}}</td>
-        <td>{{user.family_name}}</td>
-      </tr>
-      <tr v-for="pro in professions">
-        <td>{{pro.name}}</td>
-        <td>{{pro.profession_id}}</td>
-      </tr>
-    </tbody>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import {getUsers, getProfessions} from "./firebaseInit";
+import HelloWorld from './components/HelloWorld';
+
 export default {
-  name: 'app',
-  data() {
-    return {
-      usersData: [],
-      professions: []
-    };
+  name: 'App',
+
+  components: {
+    HelloWorld,
   },
-  async mounted() {
-    this.usersData = await getUsers();
-    this.professions = await getProfessions();
-  }
+
+  data: () => ({
+    //
+  }),
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import './plugins/element.js'
 
 import { firestorePlugin } from 'vuefire'
+import vuetify from './plugins/vuetify'
 Vue.use(firestorePlugin);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
